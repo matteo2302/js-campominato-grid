16fcbb420fe19171de0fdea4734db71962e1d939
matteo2302 <matteo.pizzichemi01@gmail.com>
2023-06-07 17:04:49 +0200

    not a function

--- js/script.js
+++ js/script.js
@@ -2,14 +2,18 @@ console.log("jsok");
 //fase 0
 const board = document.querySelector(".board");
 const button = document.querySelector("button");
-const cell = document.querySelector(".cell");
+
 let row = 10;
 let cols = 10;
 let totalCell = row * cols;
 button.addEventListener("click", function () {
   board.classList.remove("d-none");
   for (i = 0; i < totalCell; i++) {
-    board.innerHTML += `<div class="cell"></div>`;
-    cell.innerText += [i + 1];
+    board.innerHTML += `<div class="cell"> ${[i + 1]}</div>`;
   }
+});
+let cell = document.querySelectorAll(".cell");
+cell.addEventListener("click", function () {
+  cell.classList.add("active");
+  console.log(cell);
 });
