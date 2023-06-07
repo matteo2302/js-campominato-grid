42f41b708e985f714cee6d07a30ce90c76c52e7d
matteo2302 <matteo.pizzichemi01@gmail.com>
2023-06-07 16:15:46 +0200

    text problems

--- js/script.js
+++ js/script.js
@@ -2,6 +2,14 @@ console.log("jsok");
 //fase 0
 const board = document.querySelector(".board");
 const button = document.querySelector("button");
+const cell = document.querySelector(".cell");
+let row = 10;
+let cols = 10;
+let totalCell = row * cols;
 button.addEventListener("click", function () {
   board.classList.remove("d-none");
+  for (i = 0; i < totalCell; i++) {
+    board.innerHTML += `<div class="cell"></div>`;
+    cell.innerText += $[i + 1];
+  }
 });
--- readme.md
+++ readme.md
@@ -5,6 +5,7 @@
 - prepariamo l'html e il css dopodiche rimuoviamo le celle dal html
 - prendo in contenitore delle celle
 - prendo il btn
+- definisco il numero di caselle tramite row e cols moltiplicandoli poi insieme
 - metto in ascolto il btn
 - creo un for che giri 100 volte
 
